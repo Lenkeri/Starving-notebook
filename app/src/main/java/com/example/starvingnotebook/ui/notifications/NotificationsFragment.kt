5c94262ee913d114c173cbb626f7237a6559c635
Lenkeri <lenamalceva210@gmail.com>
2022-04-01 13:17:49 +0500

    нью

--- app/src/main/java/com/example/starvingnotebook/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/starvingnotebook/ui/notifications/NotificationsFragment.kt
@@ -31,7 +31,7 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
+        val textView: TextView = binding.textNotes
         notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
