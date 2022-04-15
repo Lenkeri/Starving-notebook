ba60a61a11955bc505c0e9e39c5ca3a54685748d
Lenkeri <lno4kamalt@yandex.ru>
2022-04-15 18:00:18 +0500

    navigations

--- app/src/main/java/com/example/starvingnotebook/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/starvingnotebook/ui/home/HomeFragment.kt
@@ -32,7 +32,7 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+       binding homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
